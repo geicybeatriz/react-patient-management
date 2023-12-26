5dd38b3c3fa847bc26062de9d1fb1e3fa35db4eb
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-12-26 12:30:12 -0300

    feat: :sparkles: add Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+function Menu({isOpen, iconPosition, onClose}) {
+
+  return (
+    <Container 
+      
+      isOpen={isOpen} 
+      top={iconPosition.top} 
+      left={iconPosition.left} 
+    >
+      <Div left={iconPosition.left}/>
+      <MenuContainer>
+        <MenuOption onClick={onClose}>Editar</MenuOption>
+        <MenuOption onClick={onClose}>Excluir</MenuOption>
+      </MenuContainer>
+    </Container>
+  )
+}
+
+export default Menu;
+
+const MenuContainer = styled.div`
+  width: 156px;
+  height: 85px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  background-color: #fff;
+  border-radius: 5px;
+  border: 1px solid #E8E8E8;
+  box-shadow: 0px 1px 15px 0px rgba(14, 30, 47, 0.03);
+`;
+
+const MenuOption = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 10px 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  border-bottom: 1px solid #E8E8E8;
+
+  color: #656565;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 21px; 
+
+  cursor: pointer;
+
+  &:hover{
+    background-color: #EDF3FC;
+    color: #136cdc;
+  }
+`;
+
+const Div= styled.div`
+  width: ${props => (props.left) ? `calc(${props.left} + 50px)` : '50px'};
+  height: 1px;
+  background-color: #ff7a00;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: ${props => (props.top) ? `calc(${props.top} + 50px)` : 0};
+  left: ${props => (props.left) ? `calc(${props.left} + 50px)` : 0};
+
+  height: 87px;
+  display: ${props => props.isOpen ? 'flex' : 'none'};
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
+  
+`;
+
