5e38907fbc99f65e210e061dea811db95c5ea306
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-12-29 15:37:36 -0300

    feat: :sparkles: enable sorting of patient data on icon click

--- src/services/patientsServices.jsx
+++ src/services/patientsServices.jsx
@@ -7,9 +7,20 @@ async function getPatientsData(){
   return promise;
 }
 
+async function fetchSortedResults(term, order){
+  const promise = await axios.get(`${BASE_URL}/patients?term=${term}&order=${order}`);
+  return promise;
+}
+
+async function addNewPatientData(data){
+  const promise = await axios.post(`${BASE_URL}/patients`, data);
+  return promise;
+}
+
 const patientsServices = {
   getPatientsData,
-
+  addNewPatientData,
+  fetchSortedResults,
 };
 
 export default patientsServices;
